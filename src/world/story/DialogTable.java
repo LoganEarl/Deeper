856eb5935eb76c236154a84fbb49c70d30a725a3
LoganEarl <iralogan42@gmail.com>
2019-03-20 17:42:14 -0700

    Quest table work

--- src/world/story/DialogTable.java
+++ src/world/story/DialogTable.java
@@ -1,18 +1,67 @@
 package world.story;
 
 import database.DatabaseManager;
+import world.entity.EntityTable;
 
+import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
-//TODO stubbed out
+/**
+ * Contains the schema for a database table used to contain dialog options
+ * @author Logan Earl
+ */
 public class DialogTable implements DatabaseManager.DatabaseTable {
+    /**The name of the table used to store dialog entries*/
+    public static final String TABLE_NAME = "dialog";
+
+    /**The unique identifier of the dialog option*/
+    public static final String DIALOG_ID = "dialogID";
+    /**The text to be said in the dialog*/
+    public static final String TEXT_TO_SAY = "textToSay";
+    /**The gesture the entity should make while speaking. Must be parsable as a gesture command*/
+    public static final String GESTURE_COMMAND = "gestureCommand";
+    /**The name of the arc that this dialog option is part of*/
+    public static final String ARC_NAME = StoryArcTable.ARC_NAME;
+    /**The minimum required story number for the associated arc to have to enable this dialog option*/
+    public static final String MIN_STORY_NUM = "minStoryName";
+    /**The maximum required story number for the associated arc to have to enable this dialog option*/
+    public static final String MAX_STORY_NUM = "maxStoryNum";
+    /**The next dialog to be made as soon as this one completes. Will wait the DIALOG_DELAY before making it*/
+    public static final String NEXT_DIALOG = "nextDialogID";
+    /**The wait in milliseconds before the next dialog is started*/
+    public static final String DIALOG_DELAY = "dialogDelay";
+    /**The quest that is offered after this dialog completes*/
+    public static final String OFFERS_QUEST_ID = "offersQuestID";
+    /**The number to be assigned to the associated arc after this dialog is complete. nullable*/
+    public static final String ARC_NUM_ON_COMPLETION = "arcNumOnCompletion";
+
+    /**A Map, containing the column names as keys and the associated data-type of the column as values*/
+    public final Map<String, String> TABLE_DEFINITION = new LinkedHashMap<>();
+
+    public DialogTable(){
+        TABLE_DEFINITION.put(DIALOG_ID, "INT PRIMARY KEY NOT NULL");
+        TABLE_DEFINITION.put(TEXT_TO_SAY, "TEXT");
+        TABLE_DEFINITION.put(GESTURE_COMMAND, "VARCHAR(32)");
+        TABLE_DEFINITION.put(ARC_NAME, String.format(Locale.US,"VARCHAR(32) FOREIGN KEY REFERENCES %s(%s)",
+                StoryArcTable.TABLE_NAME, StoryArcTable.ARC_NAME));
+        TABLE_DEFINITION.put(MIN_STORY_NUM, "INT");
+        TABLE_DEFINITION.put(MAX_STORY_NUM, "INT");
+        TABLE_DEFINITION.put(NEXT_DIALOG, String.format(Locale.US,"INT FOREIGN KEY REFERENCES %s(%s)",
+                DialogTable.TABLE_NAME, DialogTable.DIALOG_ID));
+        TABLE_DEFINITION.put(DIALOG_DELAY, "INT");
+        TABLE_DEFINITION.put(OFFERS_QUEST_ID , String.format(Locale.US,"INT FOREIGN KEY REFERENCES %s(%s)",
+                QuestTable.TABLE_NAME, QuestTable.QUEST_ID));
+        TABLE_DEFINITION.put(ARC_NUM_ON_COMPLETION, "INT");
+    }
+
     @Override
     public String getTableName() {
-        return null;
+        return TABLE_NAME;
     }
 
     @Override
     public Map<String, String> getColumnDefinitions() {
-        return null;
+        return TABLE_DEFINITION;
     }
 }
