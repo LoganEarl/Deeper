54e7246c53dd7c48e0c97447059f53dbbff611ec
LoganEarl <iralogan42@gmail.com>
2019-04-03 10:50:48 -0700

    reworked how container data is to be retrieved

--- src/world/item/ContainerStatTable.java
+++ src/world/item/ContainerStatTable.java
@@ -2,7 +2,6 @@ package world.item;
 
 import database.DatabaseManager;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
--- src/world/item/Item.java
+++ src/world/item/Item.java
@@ -6,9 +6,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 import static world.item.ItemInstanceTable.*;
-//TODO finish this. lots of work this
+//TODO finish this. lots of work this. It needs to be able to read its stats
 public class Item implements DatabaseManager.DatabaseEntry {
     private static final String CREATE_SQL = String.format(Locale.US,"INSERT INTO %s (%s %s %s %s %s) VALUES (? ? ? ? ?)",
             TABLE_NAME, ITEM_ID, ENTITY_ID, ROOM_NAME, ITEM_NAME, DISPLAY_NAME);
@@ -16,14 +19,12 @@ public class Item implements DatabaseManager.DatabaseEntry {
             TABLE_NAME, ITEM_ID);
     private static final String UPDATE_SQL = String.format(Locale.US,"UPDATE %s SET %s=?, %s=?, %s=?, %s=? WHERE %s=?",
             TABLE_NAME, ENTITY_ID, ROOM_NAME, ITEM_ID, DISPLAY_NAME, ITEM_ID);
-    private static final String GET_SQL = String.format(Locale.US,"SELECT * FROM %s WHERE %s=?",
+    private static final String GET_ID_SQL = String.format(Locale.US,"SELECT * FROM %s WHERE %s=?",
             TABLE_NAME, ITEM_ID);
     private static final String GET_NAME_SQL = String.format(Locale.US,"SELECT * FROM %s WHERE (%s=? AND %s=?)",
             TABLE_NAME, ITEM_NAME, ROOM_NAME);
     private static final String GET_OF_CONTAINER_ID_SQL = String.format(Locale.US, "SELECT * FROM (%s, %s) WHERE %s=?",
             ContainedItemTable.TABLE_NAME, ItemInstanceTable.TABLE_NAME, ContainedItemTable.CONTAINER_ID);
-    private static final String GET_OF_CONTAINER_NAME_SQL = String.format(Locale.US, "SELECT * FROM (%s, %s, %s) WHERE %s=?",
-            ContainedItemTable.TABLE_NAME, ItemInstanceTable.TABLE_NAME, ContainerInstanceTable.TABLE_NAME, ContainerInstanceTable.CONTAINER_NAME);
 
     private int itemID;
     private String entityID;
@@ -39,7 +40,7 @@ public class Item implements DatabaseManager.DatabaseEntry {
         displayName = entry.getString(DISPLAY_NAME);
     }
 
-    public static Item getItemByNameAndRoom(String itemName, String roomName, String databaseName){
+    public static Item getItemByNameRoom(String itemName, String roomName, String databaseName){
         Connection c = DatabaseManager.getDatabaseConnection(databaseName);
         PreparedStatement getSQL = null;
         Item toReturn;
@@ -64,15 +65,16 @@ public class Item implements DatabaseManager.DatabaseEntry {
         return toReturn;
     }
 
-    public static Item getItemByItemID(int itemID, String databaseName){
+
+    public static Item getItemByID(int itemID, String databaseName){
         Connection c = DatabaseManager.getDatabaseConnection(databaseName);
-        PreparedStatement getSQL = null;
+        PreparedStatement getSQL;
         Item toReturn;
         if(c == null)
             return null;
         else{
             try {
-                getSQL = c.prepareStatement(GET_SQL);
+                getSQL = c.prepareStatement(GET_ID_SQL);
                 getSQL.setInt(1,itemID);
                 ResultSet accountSet = getSQL.executeQuery();
                 if(accountSet.next())
@@ -88,9 +90,32 @@ public class Item implements DatabaseManager.DatabaseEntry {
         return toReturn;
     }
 
+    public static List<Item> getItemsOfContainerID(int containerID, String databaseName){
+        Connection c = DatabaseManager.getDatabaseConnection(databaseName);
+        PreparedStatement getSQL;
+        List<Item> foundItems = new LinkedList<>();
+        if(c == null)
+            return Collections.emptyList();
+        else{
+            try {
+                getSQL = c.prepareStatement(GET_OF_CONTAINER_ID_SQL);
+                getSQL.setInt(1,containerID);
+                ResultSet accountSet = getSQL.executeQuery();
+                while(accountSet.next()) {
+                    foundItems.add(new Item(accountSet));
+                }
+                getSQL.close();
+                c.close();
+            }catch (SQLException e){
+                foundItems = Collections.emptyList();
+            }
+        }
+        return foundItems;
+    }
+
     @Override
     public boolean saveToDatabase(String databaseName) {
-        Item item = getItemByItemID(itemID,databaseName);
+        Item item = getItemByID(itemID,databaseName);
         if(item == null)
             return DatabaseManager.executeStatement(CREATE_SQL, databaseName,
                     itemID, entityID, roomName, itemName, displayName) > 0;
@@ -111,6 +136,6 @@ public class Item implements DatabaseManager.DatabaseEntry {
 
     @Override
     public boolean existsInDatabase(String databaseName) {
-        return getItemByItemID(itemID,databaseName) != null;
+        return getItemByID(itemID,databaseName) != null;
     }
 }
